6f3150aaab937790fcc29c99d79c563df767de49
Elias Vedovato <eliasvedovato@gmail.com>
2022-10-26 22:55:45 -0300

    retocando archivos de config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://<eliasvedovato.github.io/rick_and_morty_API/',
+  base: 'https://eliasvedovato.github.io/rick_and_morty_api/',
   plugins: [react()]
 })
