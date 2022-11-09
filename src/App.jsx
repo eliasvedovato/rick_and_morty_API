5211a121489dd922deffcf8321827e35ec71b71c
Elias Vedovato <eliasvedovato@gmail.com>
2022-11-09 19:56:19 -0300

    w-full to the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { CharacterList } from './components/CharacterList'
 
 function App() {
 	return (
-		<div className='bg-dark text-white'>
+		<div className='bg-dark text-white w-full'>
 			<h1 className='text-center display-1 py-4'>Rick And Morty</h1>
 			<CharacterList />
 		</div>
